fb9d762e8dd9f1fa98ac812a282361dff8bb8442
ThuraSittNaing <coding2am@gmail.com>
2022-04-22 16:44:41 +0630

    Updates

---
+++ precache-manifest.9112177d7ed54cf14f89d85291e2d66b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9553bfe152cbd1569145",
+    "url": "/digital-business-card/static/css/main.94a2044d.chunk.css"
+  },
+  {
+    "revision": "9553bfe152cbd1569145",
+    "url": "/digital-business-card/static/js/main.9553bfe1.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/digital-business-card/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "94c69f5d5e346d52b1d5",
+    "url": "/digital-business-card/static/js/2.94c69f5d.chunk.js"
+  },
+  {
+    "revision": "5822045341b8dcbbce09",
+    "url": "/digital-business-card/static/js/runtime~main.58220453.js"
+  },
+  {
+    "revision": "94d0d7b0c38b2de7ee76e3dfeb53eee2",
+    "url": "/digital-business-card/static/media/avater.94d0d7b0.jpg"
+  },
+  {
+    "revision": "bcdd27d42b03f75c010c6ea3430c6453",
+    "url": "/digital-business-card/static/media/inter.bcdd27d4.ttf"
+  },
+  {
+    "revision": "5c05d4ae2d2f1005f52248fc78a5cf2f",
+    "url": "/digital-business-card/index.html"
+  }
+];
